ddeeae104c6d53d419f436c02983271d4177d955
alex-web-dev <alex.web-dev@mail.ru>
2025-12-25 15:36:04 +0300

    feat: update catalog page responsive layout

--- src/js/filter-menu.js
+++ src/js/filter-menu.js
@@ -1,19 +1,21 @@
 import { closeDropdown } from "./dropdown";
 
 const $filterMenuBoxes = document.querySelectorAll(".filter-menu");
+
 $filterMenuBoxes.forEach(($filterMenu) => {
   const $btnText = $filterMenu.querySelector(".filter-menu__btn-text");
-  const $inputs = $filterMenu.querySelectorAll(".filter-checkbox__input");
   const $dropdown = $filterMenu.closest(".dropdown");
 
-  $inputs.forEach(($input) => {
-    $input.addEventListener("change", () => {
-      if (!$input.checked) return;
+  $filterMenu.addEventListener("change", (event) => {
+    const $input = event.target;
 
-      const $labelText = $input.closest(".filter-checkbox").querySelector(".filter-checkbox__text-normal").innerText;
-      $btnText.innerText = $labelText;
+    if (!$input.classList.contains("filter-checkbox__input") || !$input.checked) {
+      return;
+    }
 
-      closeDropdown($dropdown);
-    });
+    const $labelText = $input.closest(".filter-checkbox").querySelector(".filter-checkbox__text-normal").innerText;
+    $btnText.innerText = $labelText;
+
+    closeDropdown($dropdown);
   });
 });
